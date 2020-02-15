0ea55114fd89d39fa6e41f8bb4763239ab5ff851
XiaoFengM <60193217+XiaoFengM@users.noreply.github.com>
2020-02-15 22:05:04 +0000

    Add files via upload

--- MScIT_TeamProject_TemplateProject/src/online/dwResources/TopTrumpsRESTAPI.java
+++ MScIT_TeamProject_TemplateProject/src/online/dwResources/TopTrumpsRESTAPI.java
@@ -59,11 +59,8 @@ public class TopTrumpsRESTAPI {
 	private int ai2Win;
 	private int ai3Win;
 	private int ai4Win;
-	private int winner; // = 0 means Human win, = 1 means Ai win
+	private int winner; // = 0 means Human win, = 1-4 means Ai 1-4 win
 	private int numDraw;
-	private int avgDraw;
-	private int largestRound;
-	private ArrayList<Integer> roundCount = new ArrayList<Integer>();
 
 
 	
@@ -242,9 +239,8 @@ public class TopTrumpsRESTAPI {
 					}
 
 					numGame++;
-					roundCount.add(round);
 
-					db.uploadGameRecord(getNumGame(), getAvgDraw(), getLargestRound(), getWinner());
+					db.uploadGameRecord(getNumGame(), getNumDraw(), getRound(), getWinner());
 
 					if(getAiNum()==4) {
 						db.uploadRoundData(getNumGame(), 0, getHumanWin());
@@ -504,15 +500,15 @@ public class TopTrumpsRESTAPI {
 	}
 
 
-	public int getNumGame() {
-		return numGame;
-	}
-
+    public int getNumGame() {
+        return numGame;
+    }
+    
 	public int getAiNum() {
 		return aiNum;
 	}
-
-	public int getAi1Win() {
+	
+    public int getAi1Win() {
 		return ai1Win;
 	}
 
@@ -531,39 +527,27 @@ public class TopTrumpsRESTAPI {
 		return ai4Win;
 	}
 
-	public int getHumanWin() {
-		return humanWin;
+    public int getHumanWin() {
+        return humanWin;
+    }
+
+    public int getWinner() {
+        return winner;
+    }
+	
+    public int getNumDraw() {
+		return numDraw;
 	}
 
-	public int getWinner() {
-		return winner;
-	}
 
-	public void resetGetter() {
-		humanWin =0;
-		ai1Win =0;
-		ai2Win =0;
-		ai3Win =0;
-		ai4Win =0;
-	}
-
-	public int getAvgDraw() {
-		if (numGame == 0){
-			avgDraw = 0;
-			return avgDraw;
-		}
-		avgDraw = Math.round(numDraw / numGame);
-		return avgDraw;
-	}
-
-	public int getLargestRound() {
-		if (numGame == 0){
-			largestRound = 0;
-			return largestRound;
-		}
-		largestRound = Collections.max(roundCount);
-		return largestRound;
-	}
+    public void resetGetter() {
+    	round = 1;
+    	humanWin =0;
+    	ai1Win =0;
+    	ai2Win =0;
+    	ai3Win =0;
+    	ai4Win =0;
+    }
 	
 	@GET
 	@Path("/helloJSONList")
